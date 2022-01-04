d1d2ffb492f5b53937a45c6ed39b1c1054a4a7db
Yehor Lohutov <39864474+LogutovEgor@users.noreply.github.com>
2022-01-04 20:38:34 +0300

    Project created

---
+++ UnityTestTinyProject/Assets/GameSystems/SpaceshipComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct SpaceshipComponent : IComponentData
+{
+    public float Speed;
+    public float AngularSpeed;
+}
---
+++ UnityTestTinyProject/Assets/GameSystems/SpaceshipMovementSystem.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.U2D.Entities.Physics;
+
+public class SpaceshipMovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+
+
+        var dt = Time.DeltaTime;
+        Entities.ForEach((ref PhysicsVelocity velocity, in SpaceshipComponent spaceshipComponent, in LocalToWorld localToWorld) => {
+            //    rot.Value = math.mul(rot.Value, quaternion.RotateY(dt * rc.Speed));
+
+            //float3 pos = translation.Value;
+
+
+            //pos.y += math.mul(spaceshipComponent.Speed, dt);
+
+            //translation.Value = pos;
+
+
+
+            float2 entityUp = new float2(localToWorld.Up.x, localToWorld.Up.y);
+
+            float speed = math.mul(spaceshipComponent.Speed, dt);
+
+            float xVelocity = math.mul(entityUp.x, speed);
+            float yVelocity = math.mul(entityUp.y, speed);
+
+
+            velocity.Linear = new float2(xVelocity, yVelocity);
+
+
+            //velocity.Linear = new float2(0, 1f);
+        }).ScheduleParallel();
+
+
+    //    float currentSpeed =
+    //motorCharacteristics.MinSpeed + ((motorCharacteristics.MaxSpeed - motorCharacteristics.MinSpeed) * enginePower);
+    //    rigidbody2D.AddForce(transform.up * currentSpeed * Time.deltaTime);
+    }
+}
